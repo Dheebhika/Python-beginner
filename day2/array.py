da07d888ae93051ae589de9b7fd6cbee6bdfc45a
Admin <admin@Intern5-MacBook-Pro.local>
2019-01-29 16:12:57 +0530

    initial commit

---
+++ day2/array.py
@@ -0,0 +1,26 @@
+# array_1 = [1,2,3,4]
+# array_1 = [1,2,3,"a"]
+# print(array_1)
+
+
+no_of_elements = int(input("Enter number of elements"))
+elements_1 = []
+
+
+def create_list():
+    for i in range(0, no_of_elements):
+        elements = int(input("Enter the elements"))
+        elements_1.append(elements)
+    print(elements_1)
+
+
+def find_even():
+    add = 0
+    for i in range(0, no_of_elements):
+        if (elements_1[i] % 2 == 0):
+            add = add + elements_1[i]
+    print(add)
+
+
+create_list()
+find_even()
