31d7d698ada9d24a995ebcbfe758ae73b8d35679
Admin <admin@Intern5-MacBook-Pro.local>
2019-01-29 10:23:53 +0530

    Initial commit

---
+++ hello world.py
@@ -0,0 +1,31 @@
+
+a = int(input("enter the number"))
+b = int(input("enter the number"))
+c = int(input("enter the number"))
+d = int(input("enter the number"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a!=b)
+print(a==b)
+if(a<b and c<d):
+    print("true")
+if(a<b or c<d):
+    print("true")
+if not(a<b and c<d):
+    print("true")
+
+a = str(input("enter the number"))
+b = str(input("enter the number"))
+print(a+b)
+
+
+
+
+
